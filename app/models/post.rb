b83054997c165f76783aa259f40b5ed95d881311
imanj12 <imanj12@gmail.com>
2018-11-02 17:35:28 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
 
+  def create
+    post = Post.new
+    post.title = params[:title]
+    post.description = params[:description]
+    post.save
+    redirect_to post_path(post.id)
+  end
+
 end
